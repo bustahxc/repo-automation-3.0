7fadba35ad6bc71b03f021edf63ccd4a39f8549f
bustahxc <87139317+bustahxc@users.noreply.github.com>
2023-08-10 10:29:00 -0400

    Update wdio.conf.ts

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -1,27 +1,24 @@
 import type { Options } from '@wdio/types'
-export const config: Options.Testrunner = {
+//import update from './dataBase/update'
+//import prueba from './dataBase/prueba'
 
-    runner: 'local',
+export const config: Options.Testrunner = {
     autoCompileOpts: {
         autoCompile: true,
         tsNodeOpts: {
-            project: './tsconfig.json',
-            transpileOnly: true
+            transpileOnly: true,
+            project: 'tsconfig.json'
         }
     },
-    
     port: 4723,
-
     specs: [
         './features/**/*.feature'
     ],
-    
     exclude: [
         // 'path/to/excluded/files'
     ],
-
-    maxInstances: 1,
-
+    maxInstances: 10,
+    runner: 'local',
     capabilities: [{
         platformName: 'Android',
         'appium:app': './apk/app-tae-release.apk',
@@ -30,29 +27,46 @@ export const config: Options.Testrunner = {
         'appium:automationName': 'UiAutomator2',
         'appium:autoGrantPermissions': true
     }],
-
-
     logLevel: 'info',
     bail: 0,
     baseUrl: 'http://localhost',
     waitforTimeout: 10000,
     connectionRetryTimeout: 120000,
     connectionRetryCount: 3,
-    services: ['appium'],
+    services: [
+        [
+            'appium',
+            {
+                args: {
+                },
+                command: 'appium'
+            }
+        ]
+    ],
     framework: 'cucumber',
-    reporters: ['spec','junit',['allure', {outputDir: 'allure-results'}],'slack'],
+    reporters: ['spec','junit',['allure', {outputDir: 'allure-results'}]],
     cucumberOpts: {
-        requireModule: ['ts-node/register'],
-        require: ['./definitions/*.ts'],
+        // <string[]> (file/dir) require files before executing features
+        require: ['./features/step-definitions/*.ts'],
+        // <boolean> show full backtrace for errors
         backtrace: false,
-        format: ['pretty'],
+        // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
+        requireModule: [],
+        // <boolean> invoke formatters without executing steps
         dryRun: false,
+        // <boolean> abort the run on first failure
         failFast: false,
+        // <boolean> hide step definition snippets for pending steps
         snippets: true,
+        // <boolean> hide source uris
         source: true,
+        // <boolean> fail if there are any undefined or pending steps
         strict: false,
+        // <string> (expression) only execute the features or scenarios with tags matching the expression
         tagExpression: '',
+        // <number> timeout for step definitions
         timeout: 60000,
+        // <boolean> Enable this config to treat undefined definitions as warnings.
         ignoreUndefinedDefinitions: false
     },
     
@@ -66,7 +80,7 @@ export const config: Options.Testrunner = {
     // resolved to continue.
     /**
      * Gets executed once before all workers get launched.
-     * @param {object} config wdio configuration object
+     * @param {Object} config wdio configuration object
      * @param {Array.<Object>} capabilities list of capabilities details
      */
     // onPrepare: function (config, capabilities) {
@@ -74,30 +88,30 @@ export const config: Options.Testrunner = {
     /**
      * Gets executed before a worker process is spawned and can be used to initialise specific service
      * for that worker as well as modify runtime environments in an async fashion.
-     * @param  {string} cid      capability id (e.g 0-0)
-     * @param  {object} caps     object containing capabilities for session that will be spawn in the worker
-     * @param  {object} specs    specs to be run in the worker process
-     * @param  {object} args     object that will be merged with the main configuration once worker is initialized
-     * @param  {object} execArgv list of string arguments passed to the worker process
+     * @param  {String} cid      capability id (e.g 0-0)
+     * @param  {[type]} caps     object containing capabilities for session that will be spawn in the worker
+     * @param  {[type]} specs    specs to be run in the worker process
+     * @param  {[type]} args     object that will be merged with the main configuration once worker is initialized
+     * @param  {[type]} execArgv list of string arguments passed to the worker process
      */
     // onWorkerStart: function (cid, caps, specs, args, execArgv) {
     // },
     /**
      * Gets executed just after a worker process has exited.
-     * @param  {string} cid      capability id (e.g 0-0)
-     * @param  {number} exitCode 0 - success, 1 - fail
-     * @param  {object} specs    specs to be run in the worker process
-     * @param  {number} retries  number of retries used
+     * @param  {String} cid      capability id (e.g 0-0)
+     * @param  {Number} exitCode 0 - success, 1 - fail
+     * @param  {[type]} specs    specs to be run in the worker process
+     * @param  {Number} retries  number of retries used
      */
     // onWorkerEnd: function (cid, exitCode, specs, retries) {
     // },
     /**
      * Gets executed just before initialising the webdriver session and test framework. It allows you
      * to manipulate configurations depending on the capability or spec.
-     * @param {object} config wdio configuration object
+     * @param {Object} config wdio configuration object
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {Array.<String>} specs List of spec file paths that are to be run
-     * @param {string} cid worker id (e.g. 0-0)
+     * @param {String} cid worker id (e.g. 0-0)
      */
     // beforeSession: function (config, capabilities, specs, cid) {
     // },
@@ -106,13 +120,13 @@ export const config: Options.Testrunner = {
      * variables like `browser`. It is the perfect place to define custom commands.
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {Array.<String>} specs        List of spec file paths that are to be run
-     * @param {object}         browser      instance of created browser/device session
+     * @param {Object}         browser      instance of created browser/device session
      */
     // before: function (capabilities, specs) {
     // },
     /**
      * Runs before a WebdriverIO command gets executed.
-     * @param {string} commandName hook command name
+     * @param {String} commandName hook command name
      * @param {Array} args arguments that command would receive
      */
     // beforeCommand: function (commandName, args) {
@@ -121,7 +135,7 @@ export const config: Options.Testrunner = {
      * Cucumber Hooks
      *
      * Runs before a Cucumber Feature.
-     * @param {string}                   uri      path to feature file
+     * @param {String}                   uri      path to feature file
      * @param {GherkinDocument.IFeature} feature  Cucumber feature object
      */
     // beforeFeature: function (uri, feature) {
@@ -130,16 +144,40 @@ export const config: Options.Testrunner = {
      *
      * Runs before a Cucumber Scenario.
      * @param {ITestCaseHookParameter} world    world object containing information on pickle and test step
-     * @param {object}                 context  Cucumber World object
+     * @param {Object}                 context  Cucumber World object
      */
-    // beforeScenario: function (world, context) {
-    // },
+    //beforeScenario: function (world, context) {
+        // Uso de la clase UpdateQR
+        /*const pages = {
+            update: update,
+            prueba: prueba
+        }
+        prueba.runApp()*/
+
+        /*const nroOperador = '17618254';
+
+        update.ejecutarUpdatesOTP(nroOperador)
+        .then(() => {
+            console.log('Updates ejecutados exitosamente');
+        })
+        .catch((error) => {
+            console.error('Error al ejecutar los updates:', error);
+        });
+
+        update.ejecutarUpdateGTD(nroOperador)
+        .then(() => {
+            console.log('Update ejecutado exitosamente');
+        })
+        .catch((error) => {
+            console.error('Error al ejecutar update:', error);
+        });*/
+    //},
     /**
      *
      * Runs before a Cucumber Step.
      * @param {Pickle.IPickleStep} step     step data
      * @param {IPickle}            scenario scenario pickle
-     * @param {object}             context  Cucumber World object
+     * @param {Object}             context  Cucumber World object
      */
     // beforeStep: function (step, scenario, context) {
     // },
@@ -148,11 +186,11 @@ export const config: Options.Testrunner = {
      * Runs after a Cucumber Step.
      * @param {Pickle.IPickleStep} step             step data
      * @param {IPickle}            scenario         scenario pickle
-     * @param {object}             result           results object containing scenario results
+     * @param {Object}             result           results object containing scenario results
      * @param {boolean}            result.passed    true if scenario has passed
      * @param {string}             result.error     error stack if scenario failed
      * @param {number}             result.duration  duration of scenario in milliseconds
-     * @param {object}             context          Cucumber World object
+     * @param {Object}             context          Cucumber World object
      */
     // afterStep: function (step, scenario, result, context) {
     // },
@@ -160,18 +198,18 @@ export const config: Options.Testrunner = {
      *
      * Runs after a Cucumber Scenario.
      * @param {ITestCaseHookParameter} world            world object containing information on pickle and test step
-     * @param {object}                 result           results object containing scenario results
+     * @param {Object}                 result           results object containing scenario results
      * @param {boolean}                result.passed    true if scenario has passed
      * @param {string}                 result.error     error stack if scenario failed
      * @param {number}                 result.duration  duration of scenario in milliseconds
-     * @param {object}                 context          Cucumber World object
+     * @param {Object}                 context          Cucumber World object
      */
     // afterScenario: function (world, result, context) {
     // },
     /**
      *
      * Runs after a Cucumber Feature.
-     * @param {string}                   uri      path to feature file
+     * @param {String}                   uri      path to feature file
      * @param {GherkinDocument.IFeature} feature  Cucumber feature object
      */
     // afterFeature: function (uri, feature) {
@@ -179,17 +217,17 @@ export const config: Options.Testrunner = {
     
     /**
      * Runs after a WebdriverIO command gets executed
-     * @param {string} commandName hook command name
+     * @param {String} commandName hook command name
      * @param {Array} args arguments that command would receive
-     * @param {number} result 0 - command success, 1 - command error
-     * @param {object} error error object if any
+     * @param {Number} result 0 - command success, 1 - command error
+     * @param {Object} error error object if any
      */
     // afterCommand: function (commandName, args, result, error) {
     // },
     /**
      * Gets executed after all tests are done. You still have access to all global variables from
      * the test.
-     * @param {number} result 0 - test pass, 1 - test fail
+     * @param {Number} result 0 - test pass, 1 - test fail
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {Array.<String>} specs List of spec file paths that ran
      */
@@ -197,7 +235,7 @@ export const config: Options.Testrunner = {
     // },
     /**
      * Gets executed right after terminating the webdriver session.
-     * @param {object} config wdio configuration object
+     * @param {Object} config wdio configuration object
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {Array.<String>} specs List of spec file paths that ran
      */
@@ -206,8 +244,8 @@ export const config: Options.Testrunner = {
     /**
      * Gets executed after all workers got shut down and the process is about to exit. An error
      * thrown in the onComplete hook will result in the test run failing.
-     * @param {object} exitCode 0 - success, 1 - fail
-     * @param {object} config wdio configuration object
+     * @param {Object} exitCode 0 - success, 1 - fail
+     * @param {Object} config wdio configuration object
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {<Object>} results object containing test results
      */
@@ -215,8 +253,8 @@ export const config: Options.Testrunner = {
     // },
     /**
     * Gets executed when a refresh happens.
-    * @param {string} oldSessionId session ID of the old session
-    * @param {string} newSessionId session ID of the new session
+    * @param {String} oldSessionId session ID of the old session
+    * @param {String} newSessionId session ID of the new session
     */
     // onReload: function(oldSessionId, newSessionId) {
     // }
